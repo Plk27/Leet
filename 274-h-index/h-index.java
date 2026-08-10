a85f0f3f5ae39c035e2b8df4048ac96c3f8070ad
Palak <145343249+Plk27@users.noreply.github.com>
2026-08-10 21:23:33 +0530

    Time: 5 ms (73.67%) | Memory: 43.7 MB (15.74%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h=0;
+        for(int i = citations.length-1; i>=0;i--){
+            if(citations[i]>h)
+                h++;
+            else
+                break;
+        }
+        return h;
+        
+    }
+}
